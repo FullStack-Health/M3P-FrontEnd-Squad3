6bd7ff3e3674fa2b56944fff44423c22d4b8fa1f
mari-martins-souza <marih_ms@hotmail.com>
2024-10-17 22:27:05 -0300

    Implementa base para autenticação do usuário no backend e conexão com a API
    - Inclusão da biblioteca para decodificação de JWT
    
    - Criação do AuthService para gerenciar autenticação do usuário
    
    - Criação do serviço de conexão com a API
    
    - Implementação do método saveUser (POST) para salvar novos usuários

---
+++ src/app/shared/services/api.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { User } from '../../models/user.model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private apiUrl = 'http://localhost:8081';
+
+  constructor(private http: HttpClient) { }
+
+  //user endpoint
+
+  saveUser(user: User): Observable<User> {
+    const jwtToken = sessionStorage.getItem('jwtToken');
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${jwtToken}`);
+
+    return this.http.post<User>(`${this.apiUrl}/users`, user, { headers });
+  }
+}
