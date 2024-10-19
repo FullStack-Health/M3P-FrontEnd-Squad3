5a3feabaf4cea8d075ee1d303c70c6d30eb391c8
mari-martins-souza <marih_ms@hotmail.com>
2024-10-19 10:35:21 -0300

    cria sidebar-menu
    - implementado menu com opções de navegação
    
    - opções de sair (voltar para tela de login) e recolher menu (lógica para recolher menu ainda não implementada)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { SidebarMenuComponent } from './shared/components/sidebar-menu/sidebar-menu.component';
 
 export const routes: Routes = [
     { path: '', component: LoginComponent },
+    { path: 'testesidebarmenu', component: SidebarMenuComponent },
 ];
