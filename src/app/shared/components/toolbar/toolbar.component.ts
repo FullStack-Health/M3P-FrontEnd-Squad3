2fea914192b9cd33236de337f3f5522d082e14d8
mari-martins-souza <marih_ms@hotmail.com>
2024-10-19 11:03:52 -0300

    cria toolbar
    - Implementado component Toolbar com título de página dinâmico, nome de usuário logado dinâmico, botão de sair (deslogar) e imagem para representar a foto do usuário

---
+++ src/app/shared/components/toolbar/toolbar.component.ts
@@ -0,0 +1,35 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../../security/service/auth.service';
+
+@Component({
+  selector: 'app-toolbar',
+  standalone: true,
+  imports: [RouterLink],
+  providers: [AuthService],
+  templateUrl: './toolbar.component.html',
+  styleUrl: './toolbar.component.scss'
+})
+export class ToolbarComponent implements OnInit {
+  pageTitle: string | undefined;
+  userName: String | null = '';
+
+  constructor(private titleService: Title, private router: Router, private authService: AuthService) { }
+
+  ngOnInit() {
+    this.pageTitle = this.titleService.getTitle();
+    this.getToken();
+  }
+
+  cleanLoggedUser() {
+    sessionStorage.setItem('jwtToken', '');
+    this.router.navigate(['/']);
+  }
+
+  getToken() {
+    const token = sessionStorage.getItem('jwtToken');
+    this.userName = this.authService.getUserNameFromToken();
+  }
+
+}
