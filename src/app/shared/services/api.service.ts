05c2f3daed4b2a4b358661a330fe91e38e07567b
mari-martins-souza <marih_ms@hotmail.com>
2024-10-19 18:49:34 -0300

    cria model, interfaces e métodos para requisições HTTP
    - Adicionado model PatientCard, interface Page para representar a estrutura de dados de uma resposta paginada e a interface DashboardStats para tratar as estatísticas do Dashboard de médicos(as) e administradores
    
    - Incluído métodos getDashboardStats (GET) e getPatientCard (GET) para exibir os dados na Dashboard de médicos(as) e administradores

--- src/app/shared/services/api.service.ts
+++ src/app/shared/services/api.service.ts
@@ -1,13 +1,16 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../../models/user.model';
+import { DashboardStats } from '../../models/dashboard-stats.interface';
+import { PatientCard } from '../../models/patient-card.model';
+import { Page } from '../../models/page.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  private apiUrl = 'https://acc5343e-2bc7-48bd-8328-985f6db75ffc.mock.pstmn.io';
+  private apiUrl = 'http://localhost:8081';
 
   constructor(private http: HttpClient) { }
 
@@ -29,6 +32,35 @@ export class ApiService {
     const headers = new HttpHeaders().set('Authorization', `Bearer ${jwtToken}`);
 
     return this.http.put<void>(`${this.apiUrl}/user`, body, { headers });
+  }
+
+  // dashboard endpoint
+
+  getDashboardStats(): Observable<DashboardStats> {
+    const jwtToken = sessionStorage.getItem('jwtToken');
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${jwtToken}`);
+
+    return this.http.get<DashboardStats>(`${this.apiUrl}/dashboard/stats`, { headers });
+  }
+
+  // patient endpoint
+
+  getPatientCard(page: number, size: number, name?: string, phone?: string, email?: string): Observable<Page<PatientCard>> {
+    const jwtToken = sessionStorage.getItem('jwtToken');
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${jwtToken}`);
+
+    let params = new HttpParams().set('page', page.toString()).set('size', size.toString());
+    if (name) {
+        params = params.set('name', name);
+    }
+    if (phone) {
+      params = params.set('phone', phone);
+    }
+    if (email) {
+        params = params.set('email', email);
+    }
+
+    return this.http.get<Page<PatientCard>>(`${this.apiUrl}/patients`, { headers, params });
   }
 
 }
