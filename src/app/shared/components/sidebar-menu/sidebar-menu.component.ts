7ea5983d851c332f59f4f688a8a031816046631e
GabrielFilomeno <biel_floripa@hotmail.com>
2024-10-20 21:42:27 -0300

    muda paleta de cores

--- src/app/shared/components/sidebar-menu/sidebar-menu.component.ts
+++ src/app/shared/components/sidebar-menu/sidebar-menu.component.ts
@@ -1,21 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar-menu',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './sidebar-menu.component.html',
   styleUrl: './sidebar-menu.component.scss'
 })
 export class SidebarMenuComponent {
-  showSidebar: boolean | undefined;
+  openClose: boolean = true;
 
   constructor(private router: Router) { }
 
   cleanLoggedUser() {
     sessionStorage.setItem('jwtToken', '');
     this.router.navigate(['']);
+  }
+
+  openCloseMenu() {
+    this.openClose = !this.openClose
   }
 
 }
