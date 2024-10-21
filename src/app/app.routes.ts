8a08848f0b9c281fcbe03ac995bd7350e2843d9f
mari-martins-souza <marih_ms@hotmail.com>
2024-10-21 20:37:27 -0300

    Implementa página de lista de prontuários
    - Criado página para listar prontuários de pacientes, com paginação
    - Disponibilizado campo de busca de pacientes por nome ou ID.
    - Adicionado botão para edição de cadastro de paciente (cadastro ainda não implementado)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { MedicalRecordListComponent } from './pages/medical-record-list/medical-record-list.component';
 
 export const routes: Routes = [
     { path: '', component: LoginComponent },
-    { path: 'dashboard', component: DashboardComponent }
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'lista-prontuarios', component: MedicalRecordListComponent }
 ];
