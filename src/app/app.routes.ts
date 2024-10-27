dc0b607759a8defe17250b21827574015e0dfbe7
Randy Gomes <raandy.gc@gmail.com>
2024-10-27 02:17:08 -0300

    feat/exam-registration: adiciona component para cadastro de Exames
    - Cria model de exam para requisições HTTP
    
    - Cria component para cadastro de exames com campo de busca por pacientes
    
    - Adiciona rota para a página de cadastro de exames
    
    - Inclui métodos CRUD no ApiService

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { LoginComponent } from './pages/login/login.component';
 import { MedicalRecordListComponent } from './pages/medical-record-list/medical-record-list.component';
 import { MedicalRecordComponent } from './pages/medical-record/medical-record.component';
 import { PatientRegistrationComponent } from './pages/patient-registration/patient-registration.component';
+import {ExamRegistrationComponent} from "./pages/exam-registration/exam-registration.component";
 
 export const routes: Routes = [
     { path: '', component: LoginComponent },
@@ -13,11 +14,13 @@ export const routes: Routes = [
     { path: 'registro-paciente', component: PatientRegistrationComponent },
     { path: 'registro-paciente/:id', component: PatientRegistrationComponent },
     {
-        path: 'prontuario', 
+        path: 'prontuario',
         children: [
           { path: ':id', component: MedicalRecordComponent }
         ]
     },
-    { path: 'registro-consulta', component: AppointmentRegistrationComponent}
-    
+    { path: 'registro-consulta', component: AppointmentRegistrationComponent},
+    { path: 'registro-exame', component: ExamRegistrationComponent}
+
+
 ];
